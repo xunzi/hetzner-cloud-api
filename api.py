46080b69019efb7e71f7b5931a0ad95f3025ab55
Christian Drexler <christian@konsolenknecht.de>
2018-02-10 18:58:06 +0100

    removed debugprints from api, fixed delete, added get_serverid

--- api.py
+++ api.py
@@ -37,35 +37,42 @@ class HetznerCloudConnection:
 
     def get(self, path):
         _resp = self.session.get("{0}/{1}".format(APIBASE, path))
-        self.debugprint(_resp.text)
+        #self.debugprint(_resp.text)
         return _resp.json()[path]
         
     def post(self,path, payload = {}):
         self.session.headers.update({"Content-Type": "application/json"})
         _resp = self.session.post("{0}/{1}".format(APIBASE, path), json = payload)
-        self.debugprint(_resp.text)
+        #self.debugprint(_resp.text)
         return _resp
 
     def delete(self,server_id):
-        s.headers.update({"Content-Type": "application/json"})
-        _resp = s.delete("{0}/servers/{1}".format(APIBASE, server_id))
-        self.debugprint(_resp.text)
+        self.session.headers.update({"Content-Type": "application/json"})
+        _resp = self.session.delete("{0}/servers/{1}".format(APIBASE, server_id))
+        #self.debugprint(_resp.text)
         return _resp
 
     def get_servers(self):
         _resp = self.get("servers")
         for _s in _resp:
             self.servers.append(_s)
-        self.debugprint("Found {0} servers".format(len(self.servers)))
-        for _s in self.servers:
-            self.debugprint("{0} - {1} - {2}".format(_s['id'], _s['name'], _s['public_net']['ipv4']['ip']))
+        #self.debugprint("Found {0} servers".format(len(self.servers)))
+        #for _s in self.servers:
+        #    self.debugprint("{0} - {1} - {2}".format(_s['id'], _s['name'], _s['public_net']['ipv4']['ip']))
 
+    def get_serverid(self, name):
+        _id = None
+        for s in self.servers:
+            if s['name'] == name:
+                _id = s['id']
+        return _id
+            
     def get_sshkeys(self):
         _resp = self.get("ssh_keys")
         #self.debugprint(_resp)
         for _key in _resp:
             self.sshkeys.append(_key)
-        self.debugprint("Found {0} keys".format(len(self.sshkeys)))
+        #self.debugprint("Found {0} keys".format(len(self.sshkeys)))
         for _key in self.sshkeys:
             self.debugprint("Key id {0}, name {1}, fingerprint {2}".format(_key['id'], _key['name'], _key['fingerprint']))
 
@@ -74,12 +81,14 @@ class HetznerCloudConnection:
         if name:
             server_spec['name'] = name
         _resp = self.post("servers", payload = server_spec)
-        self.debugprint(_resp)
+        #self.debugprint(_resp)
+        return _resp
         
 if __name__ == "__main__":
-
-    h = HetznerCloudConnection(debug=True)
-    h.debugprint(h.defaults)
-    h.debugprint(h.servers)
+    sys.stderr.write("This is meant for importing\n")
+    sys.exit(1)
+    #h = HetznerCloudConnection(debug=True)
+    #h.debugprint(h.defaults)
+    #h.debugprint(h.servers)
     #h.create_server(name="deployedviaapi")
     
